cf2114b863f90ee775623a1b4b7acc45526976a0
João Paulo S Almeida <jpalmeida@scopus.com.br>
2021-03-23 19:07:26 -0300

    Criação do MicroServices de localização

---
+++ eats-distancia-service/src/main/java/br/com/caelum/eats/distancia/Restaurante.java
@@ -0,0 +1,28 @@
+package br.com.caelum.eats.distancia;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Restaurante {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String cep;
+
+	private Boolean aprovado;
+
+	private long tipoDeCozinhaId;
+
+}
