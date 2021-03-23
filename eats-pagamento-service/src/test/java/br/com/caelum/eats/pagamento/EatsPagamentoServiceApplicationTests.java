1ed982fb80f2c931d56ce4ae15bf7676711224a1
João Paulo S Almeida <jpalmeida@scopus.com.br>
2021-03-22 22:44:01 -0300

    criacao do microservice de pagamento

---
+++ eats-pagamento-service/src/test/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caelum.eats.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatsPagamentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
